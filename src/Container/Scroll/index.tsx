4ec7f0988ad0a96867b66b62442278a2a221db4a
Leonid Kuznetsov <leonid.kuznetsov@corp.mail.ru>
2022-07-05 00:46:57 +0300

    Fixed dragable scroll

--- src/Container/Scroll/index.tsx
+++ src/Container/Scroll/index.tsx
@@ -1,4 +1,4 @@
-import React, {DragEventHandler, useState } from 'react';
+import React, {DragEventHandler, MouseEventHandler, useState} from 'react';
 
 import './Scroll.css';
 
@@ -11,16 +11,34 @@ type ScrollProps = {
 };
 
 const Scroll = ({ contentHeight, height, position, top, onScroll }: ScrollProps) => {
-    const [{ startClientY, startPosition }, setStartPosition] = useState({ startClientY: 0, startPosition: 0 });
-
     const sliderHeight = contentHeight ? Math.min(Math.max(height * height / contentHeight, 10), height) : height;
-
-    const handleShift: DragEventHandler<HTMLDivElement> = ({ clientY }) => onScroll(Math.max(Math.min(startPosition + 100 * (clientY - startClientY) / height, 100), 0));
 
     return (
         <div className="scroll" onClick={({ clientY }) => onScroll(100 * (clientY - top) / height)}>
-            <div className="slider" style={{top: `${position}%`, transform: `translate(0, ${-position}%)`, height: sliderHeight }}
-                 onDragStart={({ clientY }) => setStartPosition({startClientY: clientY, startPosition: position }) } onDrag={handleShift} onDragEnd={handleShift}/>
+            <div
+                className="slider"
+                style={{top: `${position}%`, transform: `translate(0, ${-position}%)`, height: sliderHeight }}
+                onMouseDown={({ clientY: startClientY }) => {
+                    const startPosition = position;
+                    const handleShift = ({ clientY }: MouseEvent) => {
+
+                        onScroll(Math.max(Math.min(startPosition + 100 * (clientY - startClientY) / height, 100), 0));
+                    }
+
+                    const handlerUp = () => {
+                        window.document.removeEventListener('mousemove', handleShift);
+                        window.document.removeEventListener('mouseup', handlerUp);
+                    }
+
+                    window.document.addEventListener('mousemove', handleShift, false);
+                    window.document.addEventListener('mouseup', handlerUp);
+
+                }}
+                onClick={(event) => {
+                    event.stopPropagation();
+                    event.cancelable = true;
+                }}
+            />
         </div>
     );
 };
