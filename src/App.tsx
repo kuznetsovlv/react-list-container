1a34b7c3c65e43b4f50a77b2cd87bd019bfd9818
Leonid Kuznetsov <leonid.kuznetsov@corp.mail.ru>
2022-07-05 19:33:09 +0300

    Fixed scroll bug

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { After, Before, Regular } from './Elements';
 const App = () => (
     <div className="App">
         <Container
-            list={Array.from(new Array(10000).keys()).map(index => <Regular>Item: {index}</Regular>)}
+            list={Array.from(new Array(100000).keys()).map(index => <Regular>Item: {index}</Regular>)}
             itemSize={50}
             before={<Before>Before</Before>}
             beforeSize={70}
--- src/Container/List/index.tsx
+++ src/Container/List/index.tsx
@@ -16,7 +16,7 @@ type ListProps = {
 };
 
 const List = ({list, itemSize, before, beforeSize, after, afterSize, height, position, contentHeight }: ListProps) => {
-    const shift = (contentHeight - height * position / 100) * position / 100;
+    const shift = (contentHeight - height) * position / 100;
 
     const listCoords = Math.max(shift - beforeSize, 0);
 
@@ -28,7 +28,7 @@ const List = ({list, itemSize, before, beforeSize, after, afterSize, height, pos
 
     return (
         <ul className="list">
-            {shift < beforeSize && <Item top={-shift}>{before}</Item>}
+            {shift < beforeSize && <Item top={itemShift}>{before}</Item>}
             {list.slice(from, to + 1).map((el, index) => <Item key={String(index + from)} top={itemShift}>{el}</Item>)}
             {(shift > contentHeight - height - afterSize || contentHeight - afterSize < height) && <Item top={itemShift}>{after}</Item>}
         </ul>
